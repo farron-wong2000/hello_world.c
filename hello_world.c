734a8d2b3fcde7b687dd62e9a68118c77374d04e
farron-wong2000 <159493395+farron-wong2000@users.noreply.github.com>
2024-05-07 03:28:49 +0800

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -2,6 +2,6 @@
 
 int main() {
     printf("Hello. My email address is:");
-    printf("tsz.wong-7@student.manchester.ac.uk");
+    printf("tsz.wong-7@student.manchester.ac.uk\n");
     return 0;
 }
